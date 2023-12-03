8c10f7349fdd93d4f336004b055738bfbe07fad1
SirMalcolmX <nwaf557@gmail.com>
2023-12-03 15:25:00 +0300

    fixes and refactoring for the message order

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,7 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:live_chat/constants.dart';
 
+//firebase authenticator
+final _auth = FirebaseAuth.instance;
 final _fire = FirebaseFirestore.instance;
+
+//an instance of the user logged in with FirebaseAuth
 late User loggedInUser;
 
 class ChatScreen extends StatefulWidget {
@@ -17,11 +21,8 @@ class _ChatScreenState extends State<ChatScreen> {
   //this controller is for the message to clear the text when you send the message
   final messageTextController = TextEditingController();
 
-  //firebase authenticator
-  final _auth = FirebaseAuth.instance;
-
   //an instance of the user logged in with FirebaseAuth
-  late User loggedInUser;
+  // late User loggedInUser;
 
   late String messageText;
 
@@ -29,18 +30,6 @@ class _ChatScreenState extends State<ChatScreen> {
   void initState() {
     super.initState();
     getCurrentUser();
-  }
-
-  void getCurrentUser() async {
-    try {
-      final user = await _auth.currentUser;
-      if (user != null) {
-        loggedInUser = user;
-        print(loggedInUser.email);
-      }
-    } catch (e) {
-      print(e);
-    }
   }
 
   void getMessages() async {
@@ -75,10 +64,8 @@ class _ChatScreenState extends State<ChatScreen> {
               color: kColor2,
               icon: Icon(Icons.logout),
               onPressed: () {
-                // _auth.signOut();
-                // Navigator.pop(context);
-
-                messagesStream();
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text(
@@ -118,6 +105,7 @@ class _ChatScreenState extends State<ChatScreen> {
                         _fire.collection("messages").add({
                           "sender": loggedInUser.email,
                           "text": messageText,
+                          "counterStep": DateTime.timestamp(),
                         });
                         messageTextController.clear();
                       },
@@ -140,7 +128,7 @@ class MessageStreamBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: _fire.collection("messages").snapshots(),
+      stream: _fire.collection("messages").orderBy("counterStep").snapshots(),
       builder: (context, snapshot) {
         List<MessageBubble> messageBubbles = [];
         if (!snapshot.hasData) {
@@ -150,14 +138,22 @@ class MessageStreamBuilder extends StatelessWidget {
             ),
           );
         }
-        final messages = snapshot.data!.docs;
+        final messages = snapshot.data!.docs.reversed;
         // List<Text> messageWidgets = [];
         for (var message in messages) {
           final messageText = message["text"];
           final messageSender = message["sender"];
 
+          //this is being used to check if you are the sender or not
+          final currentUser = loggedInUser.email;
+
           final messageWidget = MessageBubble(
-              sender: messageSender, text: messageText, isMe: false);
+            sender: messageSender,
+            text: messageText,
+            //will take the message sender and the current signed in user and compare
+            //if its the sender it will show a different bubble
+            isMe: currentUser == messageSender,
+          );
 
           messageBubbles.add(messageWidget);
         }
@@ -166,6 +162,7 @@ class MessageStreamBuilder extends StatelessWidget {
         // );
         return Expanded(
           child: ListView(
+            reverse: true,
             padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             children: messageBubbles,
           ),
@@ -210,13 +207,13 @@ class MessageBubble extends StatelessWidget {
                     topRight: Radius.circular(30.0),
                   ),
             elevation: 5.0,
-            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            color: isMe ? kColor1 : Colors.white,
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
               child: Text(
                 text,
                 style: TextStyle(
-                  color: isMe ? Colors.white : Colors.black54,
+                  color: isMe ? Colors.white : Colors.black,
                   fontSize: 15.0,
                 ),
               ),
@@ -225,5 +222,16 @@ class MessageBubble extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+void getCurrentUser() async {
+  try {
+    final user = await _auth.currentUser;
+    if (user != null) {
+      loggedInUser = user;
+    }
+  } catch (e) {
+    print(e);
   }
 }
