9497b43d99a723918f8379864d189889fb6ad564
SirMalcolmX <nwaf557@gmail.com>
2023-12-03 12:09:54 +0300

    finished class for the message bubble and its function

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:live_chat/constants.dart';
 
+final _fire = FirebaseFirestore.instance;
+late User loggedInUser;
+
 class ChatScreen extends StatefulWidget {
   static const String id = "chat_screen";
 
@@ -11,8 +14,8 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  final _fire = FirebaseFirestore.instance;
   late User loggedInUser;
 
   late String messageText;
@@ -90,7 +93,7 @@ class _ChatScreenState extends State<ChatScreen> {
             StreamBuilder<QuerySnapshot>(
               stream: _fire.collection("messages").snapshots(),
               builder: (context, snapshot) {
-                List<Column> messageWidgets = [];
+                List<MessageBubble> messageBubbles = [];
                 if (!snapshot.hasData) {
                   return Center(
                     child: CircularProgressIndicator(
@@ -104,20 +107,19 @@ class _ChatScreenState extends State<ChatScreen> {
                   final messageText = message["text"];
                   final messageSender = message["sender"];
 
-                  final messageWidget = Column(
-                    children: [
-                      Text("$messageSender:", style: kSenderTextStyle),
-                      Text("$messageText\n"),
-                    ],
-                  );
+                  final messageWidget = MessageBubble(
+                      sender: messageSender, text: messageText, isMe: false);
 
-                  messageWidgets.add(messageWidget);
+                  messageBubbles.add(messageWidget);
                 }
                 // return Column(
                 //   children: messageWidgets,
                 // );
-                return Column(
-                  children: messageWidgets,
+                return Expanded(
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                    children: messageBubbles,
+                  ),
                 );
               },
             ),
@@ -128,6 +130,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -143,6 +146,7 @@ class _ChatScreenState extends State<ChatScreen> {
                           "sender": loggedInUser.email,
                           "text": messageText,
                         });
+                        messageTextController.clear();
                       },
                     ),
                   ),
@@ -151,6 +155,59 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+//this is the class for the message bubble
+class MessageBubble extends StatelessWidget {
+  MessageBubble({required this.sender, required this.text, required this.isMe});
+
+  final String sender;
+  final String text;
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            sender,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.black54,
+            ),
+          ),
+          Material(
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0))
+                : BorderRadius.only(
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+            elevation: 5.0,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black54,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
